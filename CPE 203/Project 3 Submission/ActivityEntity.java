7129c462edf0230f92409a04c7ad0610d5908121
ko-ichiban <62117951+Kytoshi@users.noreply.github.com>
2025-08-12 22:08:59 -0700

    seperated projects

--- CPE 203/Project 3 Submission/ActivityEntity.java
+++
@@ -1,6 +0,0 @@
-interface ActivityEntity extends Entity{
-    void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore);
-    int getActionPeriod();
-    void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler);
-//    Action createActivityAction(WorldModel world, ImageStore imageStore);
-}
